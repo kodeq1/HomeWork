7799faba5d2a736c4512af417a51856925f8d0e0
kodeq1 <toprakbey08@gmail.com>
2025-02-27 22:36:35 +0300

    Add files via upload

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,37 +4,86 @@
 
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the
- * constants are needed, to reduce verbosity.
- */
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+
+
 public final class Constants {
   public static final class DriveConstants {
-    public static final int LEFT_LEADER_ID = 1;
-    public static final int LEFT_FOLLOWER_ID = 2;
-    public static final int RIGHT_LEADER_ID = 3;
-    public static final int RIGHT_FOLLOWER_ID = 4;
+    public static final int kLeftMotor1Port = 0;
+    public static final int kLeftMotor2Port = 1;
+    public static final int kRightMotor1Port = 2;
+    public static final int kRightMotor2Port = 3;
 
-    public static final int DRIVE_MOTOR_CURRENT_LIMIT = 60;
+    public static final int[] kLeftEncoderPorts = new int[] {0, 1};
+    public static final int[] kRightEncoderPorts = new int[] {2, 3};
+    public static final boolean kLeftEncoderReversed = false;
+    public static final boolean kRightEncoderReversed = true;
+
+    public static final double kTrackwidthMeters = 0.69;
+    public static final DifferentialDriveKinematics kDriveKinematics =
+        new DifferentialDriveKinematics(kTrackwidthMeters);
+
+    public static final int kEncoderCPR = 1024;
+    public static final double kWheelDiameterMeters = 0.15;
+    public static final double kEncoderDistancePerPulse =
+
+        (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
+
+    public static final boolean kGyroReversed = true;
+
+
+    public static final double ksVolts = 0.22;
+    public static final double kvVoltSecondsPerMeter = 1.98;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.2;
+
+
+    public static final double kvVoltSecondsPerRadian = 1.5;
+    public static final double kaVoltSecondsSquaredPerRadian = 0.3;
+
+    public static final LinearSystem<N2, N2, N2> kDrivetrainPlant =
+        LinearSystemId.identifyDrivetrainSystem(
+            kvVoltSecondsPerMeter,
+            kaVoltSecondsSquaredPerMeter,
+            kvVoltSecondsPerRadian,
+            kaVoltSecondsSquaredPerRadian);
+
+    public static final DCMotor kDriveGearbox = DCMotor.getNEO(2);
+    public static final double kDriveGearing = 8;
+
+    public static final double kPDriveVel = 0.5;
   }
 
-  public static final class RollerConstants {
-    public static final int ROLLER_MOTOR_ID = 5;
-    public static final int ROLLER_MOTOR_CURRENT_LIMIT = 60;
-    public static final double ROLLER_MOTOR_VOLTAGE_COMP = 10;
-    public static final double ROLLER_EJECT_VALUE = 0.44;
+  public static final class OIConstants {
+    public static final int kDriverControllerPort = 0;
   }
 
-  public static final class OperatorConstants {
-    public static final int DRIVER_CONTROLLER_PORT = 0;
-    public static final int OPERATOR_CONTROLLER_PORT = 1;
+  public static final class AutoConstants {
+    public static final double kMaxSpeedMetersPerSecond = 3;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+
+    public static final double kRamseteB = 2.3;
+    public static final double kRamseteZeta = 1.4;
+    public static final double kPXController = 2;
+    public static double kIXController = 0.0001;
+    public static double kDXController = 0.00001;
+
+    public static final double kPYawController = 1.7;
+    public static final double KIYawController = 0.0001;
+    public static double kDYawController = 0.00001;
+
+    public static final double kPTurnController = 0.15;
+    public static final double KITurnController = 0.000009;
+    public static double kDTurnController = 0.015;
+  }
+  public static final class FieldConstants{
+    public static final Pose2d BLUE_SUB_WOOFER = new Pose2d(new Translation2d(0, 5.60), new Rotation2d(Math.toRadians(180)));
+    public static final Pose2d RED_SUB_WOOFER = new Pose2d(new Translation2d(16.5, 5.60), new Rotation2d(Math.toRadians(0)));
   }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,6 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
 
@@ -9,70 +6,266 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
-
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.OIConstants;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotController;
+import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.wpilibj.simulation.ADXRS450_GyroSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-// Class to drive the robot over CAN
-public class CANDriveSubsystem extends SubsystemBase {
-  private final SparkMax leftLeader;
-  private final SparkMax leftFollower;
-  private final SparkMax rightLeader;
-  private final SparkMax rightFollower;
 
-  private final DifferentialDrive drive;
+public class DriveSubsystem extends SubsystemBase {
 
-  public CANDriveSubsystem() {
-    // create brushed motors for drive
-    leftLeader = new SparkMax(DriveConstants.LEFT_LEADER_ID, MotorType.kBrushed);
-    leftFollower = new SparkMax(DriveConstants.LEFT_FOLLOWER_ID, MotorType.kBrushed);
-    rightLeader = new SparkMax(DriveConstants.RIGHT_LEADER_ID, MotorType.kBrushed);
-    rightFollower = new SparkMax(DriveConstants.RIGHT_FOLLOWER_ID, MotorType.kBrushed);
 
-    // set up differential drive class
-    drive = new DifferentialDrive(leftLeader, rightLeader);
+  PS4Controller PS4Controller = new PS4Controller(OIConstants.kDriverControllerPort);
 
-    // Set can timeout. Because this project only sets parameters once on
-    // construction, the timeout can be long without blocking robot operation. Code
-    // which sets or gets parameters during operation may need a shorter timeout.
+  
+
+  private final SparkMax leftLeader = new SparkMax(1, MotorType.kBrushed);
+  private final SparkMax leftFollower = new SparkMax(2, MotorType.kBrushed);
+
+  private final SparkMax rightLeader = new SparkMax(3, MotorType.kBrushed);
+  private final SparkMax rightFollower = new SparkMax(4, MotorType.kBrushed);
+
+  private final DifferentialDrive drive = new DifferentialDrive(leftLeader, rightLeader);
+
+  private final Encoder leftEncoder = 
+        new Encoder(
+          DriveConstants.kLeftEncoderPorts[0],
+          DriveConstants.kLeftEncoderPorts[1],
+          DriveConstants.kLeftEncoderReversed
+        );
+
+  private final Encoder rightEncoder =
+        new Encoder(
+          DriveConstants.kRightEncoderPorts[0],
+          DriveConstants.kRightEncoderPorts[1],
+          DriveConstants.kRightEncoderReversed);
+
+  private final ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+        
+  double outputYcoeffector = -1;
+
+    //Odometry/kinematics definition(robot position/angle/velocity)
+    private final DifferentialDriveOdometry odometry;
+    private final DifferentialDriveKinematics m_kinematics = new DifferentialDriveKinematics(DriveConstants.kTrackwidthMeters);
+    private final PIDController xPidController = new PIDController(AutoConstants.kPXController, AutoConstants.kIXController, AutoConstants.kDXController);
+    private final PIDController yawPidController = new PIDController(AutoConstants.kPYawController, AutoConstants.KIYawController, AutoConstants.kDYawController);
+
+    public Pose2d pose;
+    public DifferentialDrivetrainSim drivetrainSimulator;
+    public EncoderSim leftEncoderSim;
+    public EncoderSim rightEncoderSim;
+    private final Field2d fieldSim;
+    private final ADXRS450_GyroSim gyroSim;
+
+    double cuurentAngle = 0;
+
+  public DriveSubsystem() {
+
+
     leftLeader.setCANTimeout(250);
     rightLeader.setCANTimeout(250);
     leftFollower.setCANTimeout(250);
     rightFollower.setCANTimeout(250);
 
-    // Create the configuration to apply to motors. Voltage compensation
-    // helps the robot perform more similarly on different
-    // battery voltages (at the cost of a little bit of top speed on a fully charged
-    // battery). The current limit helps prevent tripping
-    // breakers.
+
     SparkMaxConfig config = new SparkMaxConfig();
     config.voltageCompensation(12);
-    config.smartCurrentLimit(DriveConstants.DRIVE_MOTOR_CURRENT_LIMIT);
+    config.smartCurrentLimit(60);
 
-    // Set configuration to follow leader and then apply it to corresponding
-    // follower. Resetting in case a new controller is swapped
-    // in and persisting in case of a controller reset due to breaker trip
+    //left motors config
     config.follow(leftLeader);
     leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     config.follow(rightLeader);
     rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-    // Remove following, then apply config to right leader
+    //right motors config
     config.disableFollowerMode();
     rightLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    // Set conifg to inverted and then apply to left leader. Set Left side inverted
-    // so that postive values drive both sides forward
     config.inverted(true);
     leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  }
 
-  @Override
+    //encoder config
+    leftEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
+    rightEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
+
+    resetEncoders();
+
+    odometry = 
+    new DifferentialDriveOdometry(
+        Rotation2d.fromDegrees(getHeading()),
+                leftEncoder.getDistance(),
+                rightEncoder.getDistance());
+
+      if (RobotBase.isSimulation()) {
+        drivetrainSimulator = new DifferentialDrivetrainSim(
+          DriveConstants.kDrivetrainPlant, 
+          DriveConstants.kDriveGearbox, 
+          DriveConstants.kDriveGearing, 
+          DriveConstants.kTrackwidthMeters, 
+          DriveConstants.kWheelDiameterMeters / 2.0, 
+          VecBuilder.fill(0, 0, 0.0001, 0.1, 0.1, 0.005, 0.005));
+          
+          leftEncoderSim = new EncoderSim(leftEncoder);
+          rightEncoderSim = new EncoderSim(rightEncoder);
+          gyroSim = new ADXRS450_GyroSim(gyro);
+          fieldSim = new Field2d();
+          SmartDashboard.putData("Field", fieldSim);
+            
+      } else {
+          leftEncoderSim = null;
+          rightEncoderSim = null;
+          gyroSim = null;
+          fieldSim = null;
+      }
+        
+   }
+  
+                  
+    @Override
   public void periodic() {
+        //Updates odometry
+        pose = odometry.update(
+            Rotation2d.fromDegrees(getHeading()), 
+            leftEncoder.getDistance(), 
+            rightEncoder.getDistance());
+
+        //Updates robots pose
+        fieldSim.setRobotPose(pose);
+
   }
 
-  // sets the speed of the drive motors
-  public void driveArcade(double xSpeed, double zRotation) {
-    drive.arcadeDrive(xSpeed, zRotation);
+  public void simulationPeriodic() {
+    //sets simulaton motors input to real motors
+    drivetrainSimulator.setInputs(
+        leftLeader.get() * RobotController.getBatteryVoltage(), 
+        rightLeader.get() * RobotController.getBatteryVoltage());
+    drivetrainSimulator.update(0.02);
+
+    //sets sim encoders distance and velocity in meters
+    leftEncoderSim.setDistance(drivetrainSimulator.getLeftPositionMeters());
+    leftEncoderSim.setRate(drivetrainSimulator.getLeftVelocityMetersPerSecond());
+
+    rightEncoderSim.setDistance(drivetrainSimulator.getRightPositionMeters());
+    rightEncoderSim.setRate(drivetrainSimulator.getRightVelocityMetersPerSecond());
+
+    //sets simulation gyros angle 
+    //(-) because .getheading() is counterclockwise positive and setangle is clockwise positive
+    gyroSim.setAngle(-drivetrainSimulator.getHeading().getDegrees());
+    
   }
+
+
+
+  SlewRateLimiter outputYLimiter = new SlewRateLimiter(0.5);
+  double outputY;
+  double outputX;
+
+  public void arcadeDrive(double forward, double rotation) {
+    outputY = forward*0.8;
+      outputX = rotation*0.55;
+    if(PS4Controller.getR1Button()){
+      outputY = outputYLimiter.calculate(outputYcoeffector*forward*0.9);
+      outputX = rotation*0.55;
+    }
+      
+    drive.arcadeDrive(outputY, outputX);
+  }
+
+
+  public void tankDriveVolts(double leftVolts, double rightVolts) {
+    leftLeader.setVoltage(leftVolts);
+    rightLeader.setVoltage(rightVolts);
+    drive.feed();
+  }
+
+
+  public void drive(ChassisSpeeds speeds){
+    drive.feed();  
+
+    xPidController.setSetpoint(speeds.vxMetersPerSecond);
+    yawPidController.setSetpoint(speeds.omegaRadiansPerSecond);
+
+    drive.arcadeDrive(xPidController.calculate(getChassisSpeeds().vxMetersPerSecond), yawPidController.calculate(getChassisSpeeds().omegaRadiansPerSecond));
+  }
+
+  public double getAverageEncoderDistance() {
+    return (leftEncoder.getDistance() + rightEncoder.getDistance()) / 2.0;
+  }
+
+  public void setMaxOutput(double maxOutput) {
+    drive.setMaxOutput(maxOutput);
+  }
+
+  //sets robots heading to zero
+  public void zeroHeading() {
+    gyro.reset();
+  }
+
+
+  //reset functions
+      
+  private void resetEncoders() {
+    leftEncoder.reset();
+    rightEncoder.reset();
+
+  }
+
+  public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+    drivetrainSimulator.setPose(pose);
+    odometry.resetPosition(
+        Rotation2d.fromDegrees(pose.getRotation().getDegrees()),
+        leftEncoder.getDistance(),
+        rightEncoder.getDistance(),
+        pose);
+  }
+
+
+  //getters
+
+  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    return new DifferentialDriveWheelSpeeds(leftEncoder.getRate(), rightEncoder.getRate());
 }
+
+  public ChassisSpeeds getChassisSpeeds(){
+    return m_kinematics.toChassisSpeeds(getWheelSpeeds());
+  }
+
+  public Pose2d getPose() {
+    return odometry.getPoseMeters();
+}
+
+  private double getHeading() {
+    return Math.IEEEremainder(gyro.getAngle(), 360) * (DriveConstants.kGyroReversed ? -1.0 : 1.0);
+}
+
+public Encoder getLeftEncoder() {
+  return leftEncoder;
+}
+
+public Encoder getRightEncoder() {
+  return rightEncoder;
+}
+
+}
+
